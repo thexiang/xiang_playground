28851d216214b8aa9c8339a69c64254c58a6a776
Xiang Xu <shawn.xu@datarobot.com>
2022-02-21 20:22:16 -0500

    nodejs init

---
+++ services/dogs/seed_dev.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+// A `main` function so that you can use async/await
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const user = await prisma.dog.create({
+    data: {
+      name: 'Miya',
+    },
+  })
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
